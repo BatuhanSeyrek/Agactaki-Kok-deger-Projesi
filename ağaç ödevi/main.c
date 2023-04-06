d5ed6542dabf5375690f132d01cb1d42ccca5ac7
BatuhanSeyrek <115466293+BatuhanSeyrek@users.noreply.github.com>
2023-04-06 22:20:23 +0300

    update
    update

--- ağaç ödevi/main.c
+++ ağaç ödevi/main.c
@@ -3,56 +3,56 @@
 
 // Düðüm yapýsý
 struct Dugum {
-    int deger;          // Düðümün deðerini tanýmladýk
-    struct Dugum* sol;  // Sol alt düðüm deðrini tanýmladýk ve oradaki '*' ifadesi iþaretçidir
-    struct Dugum* sag;  // Sað alt düðüm deðerini tanýmladýk ve oradaki '*' ifadesi iþaretçidir
+    int deger;          // Dugumun degerini tanimladik
+    struct Dugum* sol;  // Sol alt dugum degrini tanimladik ve oradaki '*' ifadesi isaretcidir
+    struct Dugum* sag;  // Sag alt dugum degerini tanimladik ve oradaki '*' ifadesi isaretcidir
 };
 
 
-struct Dugum* dugumOlustur(int deger) {// Yeni bir düðüm oluþturur ve geri döndürür 
-    struct Dugum* dugum = (struct Dugum*) malloc(sizeof(struct Dugum));//burdada malloc yasýný kullandýk
-    dugum->deger = deger;//düðümdeki deðeri ,deðer deðiþkeni ile eþitledik
-    dugum->sol = NULL;//düðümdeki solu ,sýfýr deðerine eþitledik
-    dugum->sag = NULL;//düðümdeki saðý ,sýfýr deðerine eþitledik
-    return dugum;//düðüm deðerinin dönmesini saðlar
+struct Dugum* dugumOlustur(int deger) {// Yeni bir dugum olusturur ve geri dondurur 
+    struct Dugum* dugum = (struct Dugum*) malloc(sizeof(struct Dugum));//burdada malloc yasini kullandik
+    dugum->deger = deger;//dugumdeki degeri ,deger degiskeni ile esitledik
+    dugum->sol = NULL;//dugumdeki solu ,sýfýr degerine esitledik
+    dugum->sag = NULL;//dugumdeki sagý ,sýfýr degerine esitledik
+    return dugum;//dugum degerinin donmesini saglar
 }
 
 
-int altDugumToplami(struct Dugum* kok) {// Kök deðer alt düðümlerin toplamý mý? ifadesini cevabýný bize verecek asýl yer burasý
-    if (kok == NULL || (kok->sol == NULL && kok->sag == NULL)) {//kök sýfýra eþit ve kodeki 
-        // Eðer kök düðüm yoksa veya kök düðüm yaprak düðümse,
-        // kök deðer alt düðümlerin toplamýdýr.
-        return 1;//bir deðeri döndürür
+int altDugumToplami(struct Dugum* kok) {// Kok deger alt dugumlerin toplami mi? ifadesini cevabini bize verecek asil yer burasi
+    if (kok == NULL || (kok->sol == NULL && kok->sag == NULL)) {//kok sifira esit ve kodeki 
+        // Eger kok dugum yoksa veya kok dugum yaprak dugumse,
+        // kok deger alt dugumlerin toplamidir.
+        return 1;//bir degeri dondurur
     } else {
-        int toplam = 0;//toplam deðeri sýfýr deðerine eþitledik
-        if (kok->sol != NULL) {//kökdeki sol deðeri sýfýra eþit deðilse bu durum gerçekleþir
-            toplam += kok->sol->deger;  // Sol alt düðümün deðerini toplamý
+        int toplam = 0;//toplam degeri sifir degerine esitledik
+        if (kok->sol != NULL) {//kokdeki sol degeri sifira esit degilse bu durum gerceklesir
+            toplam += kok->sol->deger;  // Sol alt dugumun degerini toplami
         }
         if (kok->sag != NULL) {
-            toplam += kok->sag->deger;  // Sað alt düðümün deðerini toplamý
+            toplam += kok->sag->deger;  // Sag alt dugumun degerini toplami
         }
-        // Kök düðümün deðeri alt düðümlerin toplamýna eþit mi?
-        // Sol ve sað alt düðümlerin de alt düðümleri ayný þekilde kontrol edilir.
+        // Kok dugumun degeri alt dugumlerin toplamina esit mi?
+        // Sol ve sag alt dugumlerin de alt dugumleri ayni sekilde kontrol edilir.
         return (kok->deger == toplam && altDugumToplami(kok->sol) && altDugumToplami(kok->sag));
     }
 }
 
 int main() {
-    // kulanýcýnýn deðer giribileceði þekilde hazýrladým
+    // kulanicinin deger giribilecegi sekilde hazirladim
     int kok_deger, sol_deger, sag_deger;
-    printf("Kok degerini girin: ");//kullanýcýdan kök deðeri alýnýr 
+    printf("Kok degerini girin: ");//kullanicidan kok degeri alinir 
     scanf("%d", &kok_deger);
-    printf("Sol alt dugum degerini girin: ");//kullanýcýdan sol alt düðüm deðeri alýnýr 
+    printf("Sol alt dugum degerini girin: ");//kullanicidan sol alt dugum degeri alinir 
     scanf("%d", &sol_deger);
-    printf("Sag alt dugum degerini girin: ");//kullanýcýdan sað alt deðer düðüm deðeri alýnýr 
+    printf("Sag alt dugum degerini girin: ");//kullanicidan sað alt deger dugum degeri alinir 
     scanf("%d", &sag_deger);
 
-    struct Dugum* kok = dugumOlustur(kok_deger);//dugum*kok ,dugumoluþtur(kok_deger)'e eþitledik
-    kok->sol = dugumOlustur(sol_deger);//kokdeki sol deðerine dugumoluþtur(sol_deger)'eþitledik
-    kok->sag = dugumOlustur(sag_deger);//kokdeki sað deðerine dugumoluþtur(sað_deger)'eþitledik
+    struct Dugum* kok = dugumOlustur(kok_deger);//dugum*kok ,dugum olustur(kok_deger)'e esitledik
+    kok->sol = dugumOlustur(sol_deger);//kokdeki sol deðerine dugum olustur(sol_deger)'esitledik
+    kok->sag = dugumOlustur(sag_deger);//kokdeki sag degerine dugum olustur(sag_deger)'esitledik
 
     
-    if (altDugumToplami(kok)) {// Kontrol et ve sonucu yazdýr
+    if (altDugumToplami(kok)) {// Kontrol et ve sonucu yazdir
         printf("Kok deger alt dugumlerin toplamidir.\n");
     } else {
         printf("Kok deger alt dugumlerin toplami degildir.\n");
